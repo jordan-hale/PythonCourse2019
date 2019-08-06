b839985e9d5927e6a26f7c46538f45c940e02cbc
jordan-hale <53221177+jordan-hale@users.noreply.github.com>
2019-08-05 19:08:20 -0500

    Class progress
    Accidentally deleted files in class when changing folders (but this is the point I had reached)

--- day01/exercise01_jordan.py
+++ day01/exercise01_jordan.py
@@ -12,56 +12,16 @@ for i in range(1, 11):
 
 fibonacci
 
-fibonacci = [1, 1]
-while len(fibonacci) < 10:
-    fibonacci.append(fibonacci[-1] + fibonacci[-2])
-
-fibonacci
-
-def findn(n):
-    fibonacci = []
-    while len(fibonacci) < n:
-        if len(fibonacci) < 2: fibonacci.append(1)
-        else: fibonacci.append(sum(fibonacci[-2:]))
-    return fibonacci
-
-findn(10)
-
-
-
-
 """return true if there is no e in 'word', else false"""
-def has_no_e(word):
-    return 'e' not in word
-
-[has_no_e('abcd'), has_no_e('abcde')]
-
 
 
 """return true if there is e in 'word', else false"""
-def has_e(word):
-    return not has_no_e(word)
-
-[has_e('abcd'), has_e('abcde')]
 
 
 """return true if word1 contains only letters from word2, else false"""
-def uses_only(word1, word2):
-    return not False in [i in word2 for i in [j for j in word1]]
-
-[uses_only('abcd', 'abcde'), uses_only('abcde', 'abcd')]
 
 
 """return true if word1 uses all the letters in word2, else false"""
-def uses_all(word1, word2):
-    return not False in [i in word1 for i in [j for j in word2]]
-
-[uses_all('abcd', 'abcde'), uses_all('abcde', 'abcd')]
 
 
 """true/false is the word in alphabetical order?"""
-def is_abecedarian(word):
-    original = [i for i in word]
-    return sorted(original) == original
-
-[is_abecedarian('abcde'), is_abecedarian('adcbe')]
--- day01/lab01_jordan.py
+++ day01/lab01_jordan.py
@@ -20,35 +20,13 @@ def int_to_base(num, base):
 
 
 """take a string-formatted number and its base and return the base-10 integer"""
-def base_to_int(string, base):
-	powers = [int(i) for i in list(string)][::-1]
-	answer = 0
-	for i in range(len(powers)):
-		answer += powers[i]*base**i
-	return answer
-
 
 """add two numbers of different bases and return the sum"""
-def flexibase_add(str1, str2, base1, base2):
-	return base_to_int(str1, base1) + base_to_int(str2, base2)
-
 
 """multiply two numbers of different bases and return the product"""
-def flexibase_multiply(str1, str2, base1, base2):
-	return base_to_int(str1, base1) * base_to_int(str2, base2)
-
 
 """given an integer, return the Roman numeral version"""
-def romanify(num):
-	numerals = {1 : 'I', 4 : 'IV', 5 : 'V', 9: 'IX', 10 : 'X', 40 : 'XL', 50 : 'L', 90 : 'XC', 100 : 'C', 400 : 'CD', 500 : 'D', 900 : 'CM', 1000 : 'M'}
-	required = []
-	for i in sorted(numerals.keys())[::-1]:
-		while num >= i:
-			required.append(i)
-			num -= i
-	return ''.join(numerals[i] for i in required)
 
-romanify(1234)
 
 # Copyright (c) 2014 Matt Dickenson
 #
