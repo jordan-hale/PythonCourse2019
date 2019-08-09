c8674be607a00a87bf2fb53a00ba105c36ad4490
jordan-hale <53221177+jordan-hale@users.noreply.github.com>
2019-08-08 20:44:33 -0500

    Update hw1_jordan.py
    Did everything involving separate domains...have yet to figure out how to draw money from one source to pay a given value of another item and then add the respecting quantity

--- homeworks/hw1/hw1_jordan.py
+++ homeworks/hw1/hw1_jordan.py
@@ -1,45 +1,79 @@
 #portfolio = Portfolio() #Creates a new portfolio
 #portfolio.addCash(300.50) #Adds cash to the portfolio
+#portfolio.withdrawCash(50) #Removes $50
 
+#CODE:
 class Portfolio:
     def __init__(self, cash, stock, mutualfund):
         self.cash = cash
         self.stock = stock
         self.mutualfund = mutualfund
-    def addCash(self, cash):
-        self.cash = int(self.cash) + cash
+    def addCash(self, a_cash):
+        self.cash = self.cash + a_cash
+    def withdrawCash(self, w_cash):
+        self.cash = self.cash - w_cash
 
-    #portfolio = Portfolio(26, 2, 18)
-    #print(portfolio.cash)
-    #print(portfolio.stock)
-    #print(portfolio.mutualfund)
-    #portfolio.addCash(300.50)
-    #print(portfolio.cash)
+#COMMANDS:
+portfolio = Portfolio(0, 0, 0)
+print(portfolio.cash)
+print(portfolio.stock)
+print(portfolio.mutualfund)
+portfolio.addCash(300.50)
+print(portfolio.cash)
+portfolio.withdrawCash(50)
+print(portfolio.cash)
 
-s = Stock(20, "HFH") #Create Stock with price 20 and symbol "HFH"
 
-portfolio.buyStock(5, s) #Buys 5 shares of stock s
+#s = Stock(20, "HFH") #Create Stock with price 20 and symbol "HFH"
 
-mf1 = MutualFund("BRT") #Create MF with symbol "BRT"
+#CODE:
+class Stock:
+    def __init__(self, price, s_symbol):
+        self.price = price
+        self.s_symbol = s_symbol
 
-mf2 = MutualFund("GHT") #Create MF with symbol "GHT"
+#COMMANDS:
+s = Stock(20, "HFH")
+print(s.price)
+print(s.s_symbol)
 
+
+#mf1 = MutualFund("BRT") #Create MF with symbol "BRT"
+#mf2 = MutualFund("GHT") #Create MF with symbol "GHT"
+
+#CODE:
+class MutualFund:
+    def __init__(self, mf_symbol):
+        self.mf_symbol = mf_symbol
+
+#COMMANDS:
+mf1 = MutualFund("BRT")
+print(mf1.mf_symbol)
+mf2 = MutualFund("GHT")
+print(mf2.mf_symbol)
+
+
+#portfolio.buyStock(5, s) #Buys 5 shares of stock s
+portfolio.buyStock(5, s)
+
+#portfolio.buyMutualFund(10.3, mf1) #Buys 10.3 shares of "BRT"
+#portfolio.buyMutualFund(2, mf2) #Buys 2 shares of "GHT"
 portfolio.buyMutualFund(10.3, mf1) #Buys 10.3 shares of "BRT"
-
 portfolio.buyMutualFund(2, mf2) #Buys 2 shares of "GHT"
 
+#print(portfolio) #Prints portfolio
 print(portfolio) #Prints portfolio
+    #cash: $140.50
+    #stock: 5 HFH
+    #mutual funds: 10.33 BRT
+    # 2 GHT
 
-#cash: $140.50
-#stock: 5 HFH
-#mutual funds: 10.33 BRT
-# 2 GHT
-
+#portfolio.sellMutualFund("BRT", 3) #Sells 3 shares of BRT
 portfolio.sellMutualFund("BRT", 3) #Sells 3 shares of BRT
 
+#portfolio.sellStock("HFH", 1) #Sells 1 share of HFH
 portfolio.sellStock("HFH", 1) #Sells 1 share of HFH
 
-portfolio.withdrawCash(50) #Removes $50
-
+#portfolio.history() #Prints a list of all transactions
 portfolio.history() #Prints a list of all transactions
-#ordered by time
+    #ordered by time
